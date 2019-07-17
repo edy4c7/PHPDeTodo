336f3b6ec5540617359bd23a8b937a3e5e42de2c
edy4c7 <edy4c7@gmail.com>
2019-07-17 21:42:27 +0900

    install bootstrap

--- src/index.php
+++ src/index.php
@@ -1,35 +1,48 @@
 <?php
-    require_once('models/todo.php'); 
-    require_once('db.php');
-    try {
-        $items = Todo::all(getPDO());
-    } catch (PDOException $e) {
-        header('Content-Type: text/plain; charset=UTF-8', true, 500);
-        exit();
-    }
+require_once('models/todo.php');
+require_once('db.php');
+try {
+    $items = Todo::all(getPDO());
+} catch (PDOException $e) {
+    header('Content-Type: text/plain; charset=UTF-8', true, 500);
+    exit();
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>php de todo</title>
 </head>
+
 <body>
-    <form action="create.php" method="post">
-        <input type="text" name="text" id="text">
-        <button type="submit" name="submit">Add</button>
-    </form>
-    <ul>
-        <?php foreach($items as $item): ?>
-        <li>
-            <span class="<?= $item->isCompleted ? 'completed' : ''?>"><?= htmlspecialchars($item->text); ?></span>
-            <a href="update.php?id=<?= $item->id?>"><?= $item->isCompleted ? 'undone': 'done'?></a>
-            <a href="delete.php?id=<?= $item->id?>">delete</a>
-        </li>
-        <?php endforeach; ?>
-    </ul>
+    <div class="container">
+        <h1>PHP De Todo</h1>
+        <form action="create.php" method="post">
+            <input type="text" name="text" id="text">
+            <button type="submit" name="submit">Add</button>
+        </form>
+        <ul>
+            <?php foreach ($items as $item) : ?>
+                <li>
+                    <?php if ($item->isCompleted): ?>
+                        <del><?= htmlspecialchars($item->text); ?></del>
+                    <?php else: ?>
+                        <?= htmlspecialchars($item->text); ?>
+                    <?php endif ?>
+                    <a href="update.php?id=<?= $item->id ?>"><?= $item->isCompleted ? 'undone' : 'done' ?></a>
+                    <a href="delete.php?id=<?= $item->id ?>">delete</a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
+
 </html>
